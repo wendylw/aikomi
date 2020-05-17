bacd34b347da6970a6abe342a868467179386d2b
wendylw <wendylw2013@gmail.com>
2020-05-17 10:17:33 +0800

    Update news models

---
+++ models/News.js
@@ -0,0 +1,56 @@
+var keystone = require('keystone');
+var constants = require('../lib/constants');
+var fileStorage = require('./common-util/file-storage');
+var slug = require('limax');
+
+var Types = keystone.Field.Types;
+
+/**
+ * News Model
+ * ===========
+ */
+
+var News = new keystone.List('News', {
+  autokey: {
+    from: 'slug',
+    path: 'key'
+  },
+  map: { name: 'label' },
+  label: 'News',
+});
+
+News.add({
+  slug: {
+    type: String,
+    noedit: true,
+    unique: true
+  },
+  label: {
+    type: String,
+    noedit: true,
+    initial: false,
+    unique: true
+  },
+  country: {
+    type: Types.Select,
+    options: Object.keys(constants.COUNTRIES),
+    noedit: true,
+    initial: true,
+    required: true
+  },
+  bannerImage: {
+    type: Types.File,
+    thumb: true,
+    storage: fileStorage.storage('news/')
+  }
+});
+
+News.schema.pre('save', function (next) {
+  this.label = `${this.pageName} (${this.country})`;
+  this.slug = slug(`${this._id}-(${this.country})`);
+  next();
+});
+
+News.defaultColumns = 'label, title';
+
+News.register();
